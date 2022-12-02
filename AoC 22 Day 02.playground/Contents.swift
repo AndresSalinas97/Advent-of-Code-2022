a97aa572f52c419faddeaea5096280ecf1a2d3d2
AndresSalinas97 <andressalinas97@outlook.com>
2022-12-02 13:42:36 +0100

    Part Two Winner

--- AoC 22 Day 02.playground/Contents.swift
+++ AoC 22 Day 02.playground/Contents.swift
@@ -10,7 +10,7 @@ let fileURL = Bundle.main.url(forResource: "input", withExtension: "txt")
 let input = try String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
 
 var rounds = input.components(separatedBy: "\n")
-while (rounds.last!.isEmpty) {
+while rounds.last!.isEmpty {
     rounds.removeLast() // Removes the empty lines at the end (if any)
 }
 
@@ -38,27 +38,29 @@ func getRoundScore(hisChoice: Shape, myChoice: Shape) -> Int {
     return myChoice.rawValue
 }
 
+// MARK: - PART ONE
+
 func partOne() -> Int {
     var totalScore = 0
 
     for round in rounds {
-        let hisChoiceString = round[round.startIndex]
-        let myChoiceString = round[round.index(before: round.endIndex)]
+        let hisChoiceChar = round[round.startIndex]
+        let myChoiceChar = round[round.index(before: round.endIndex)]
 
         let hisChoice: Shape
         let myChoice: Shape
 
-        if hisChoiceString == "A" {
+        if hisChoiceChar == "A" {
             hisChoice = .rock
-        } else if hisChoiceString == "B" {
+        } else if hisChoiceChar == "B" {
             hisChoice = .paper
         } else {
             hisChoice = .scissors
         }
 
-        if myChoiceString == "X" {
+        if myChoiceChar == "X" {
             myChoice = .rock
-        } else if myChoiceString == "Y" {
+        } else if myChoiceChar == "Y" {
             myChoice = .paper
         } else {
             myChoice = .scissors
@@ -71,3 +73,60 @@ func partOne() -> Int {
 }
 
 print("PART ONE: \(partOne())")
+
+// MARK: - PART TWO
+
+func partTwo() -> Int {
+    var totalScore = 0
+
+    for round in rounds {
+        let hisChoiceChar = round[round.startIndex]
+        let myStrategyChar = round[round.index(before: round.endIndex)]
+
+        let hisChoice: Shape
+        let myChoice: Shape
+
+        if hisChoiceChar == "A" {
+            hisChoice = .rock
+        } else if hisChoiceChar == "B" {
+            hisChoice = .paper
+        } else {
+            hisChoice = .scissors
+        }
+
+        // Lose Strategy
+        if myStrategyChar == "X" {
+            switch hisChoice {
+            case .rock:
+                myChoice = .scissors
+            case .paper:
+                myChoice = .rock
+            case .scissors:
+                myChoice = .paper
+            }
+        }
+
+        // Draw Strategy
+        else if myStrategyChar == "Y" {
+            myChoice = hisChoice
+        }
+
+        // Win Strategy
+        else {
+            switch hisChoice {
+            case .rock:
+                myChoice = .paper
+            case .paper:
+                myChoice = .scissors
+            case .scissors:
+                myChoice = .rock
+            }
+        }
+
+        totalScore += getRoundScore(hisChoice: hisChoice, myChoice: myChoice)
+    }
+
+    return totalScore
+}
+
+print("PART TWO: \(partTwo())")
