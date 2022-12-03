75950c6f851cfb589d586d6ebde6606738c02313
AndresSalinas97 <andressalinas97@outlook.com>
2022-12-03 18:19:04 +0100

    Add template

---
+++ Day-XX.playground/Contents.swift
@@ -0,0 +1,13 @@
+//
+//  Advent of Code 2022 - Day XX: XXXXXXXXXXXXXX
+//
+//  Created by Andrés Salinas Lima on XX/12/22
+//
+
+import Cocoa
+
+let fileURL = Bundle.main.url(forResource: "sample", withExtension: "txt")
+let input = try String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
+
+print("Hello World!")
+
