630b594c60f430842e967b666b313cf4eb02a001
AndresSalinas97 <andressalinas97@outlook.com>
2022-12-06 13:40:16 +0100

    Update template

--- Day-XX.playground/Contents.swift
+++ Day-XX.playground/Contents.swift
@@ -1,30 +1,14 @@
 //
-//  Advent of Code 2022 - Day XX: XXXXXXXXXXXXXX
+//  Advent of Code 2022 - Day XX: XXXXX
 //
 //  Created by Andrés Salinas Lima on XX/12/22
 //
 
 import Cocoa
 
-let fileURL = Bundle.main.url(forResource: "sample", withExtension: "txt")
-let input = try String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
+let fileURL = Bundle.main.url(forResource: "sample", withExtension: "txt")!
+let input = try String(contentsOf: fileURL, encoding: String.Encoding.utf8)
 
-// MARK: - PART ONE
-
-func PartOne() -> Int {
-    var result = 0
-
-    return result
+input.enumerateLines { line, _ in
+    <#code#>
 }
-
-print("PART ONE: \(PartOne())")
-
-// MARK: - PART TWO
-
-func PartTwo() -> Int {
-    var result = 0
-
-    return result
-}
-
-print("PART TWO: \(PartTwo())")
