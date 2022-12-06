409980508126c594055b38d3144283d2f1dfb934
AndresSalinas97 <andressalinas97@outlook.com>
2022-12-06 12:26:44 +0100

    Part two solved

--- Day-04.playground/Contents.swift
+++ Day-04.playground/Contents.swift
@@ -14,38 +14,27 @@ while elfPairs.last!.isEmpty {
     elfPairs.removeLast() // Removes the empty lines at the end (if any)
 }
 
-// MARK: - PART ONE
+var resultPartOne = 0
+var resultPartTwo = 0
 
-func PartOne() -> Int {
-    var result = 0
+for elfPair in elfPairs {
+    let assignments = elfPair.components(separatedBy: ",")
 
-    for elfPair in elfPairs {
-        let assignments = elfPair.components(separatedBy: ",")
-        
-        let elf1Range = assignments[0].components(separatedBy: "-")
-        let elf2Range = assignments[1].components(separatedBy: "-")
+    let elf1Range = assignments[0].components(separatedBy: "-")
+    let elf2Range = assignments[1].components(separatedBy: "-")
 
-        let elf1start = Int(elf1Range[0])!
-        let elf1end = Int(elf1Range[1])!
-        let elf2start = Int(elf2Range[0])!
-        let elf2end = Int(elf2Range[1])!
+    let elf1start = Int(elf1Range[0])!
+    let elf1end = Int(elf1Range[1])!
+    let elf2start = Int(elf2Range[0])!
+    let elf2end = Int(elf2Range[1])!
 
-        if (elf2start >= elf1start && elf2end <= elf1end) || (elf1start >= elf2start && elf1end <= elf2end) {
-            result += 1
-        }
+    if (elf2start >= elf1start && elf2end <= elf1end) || (elf1start >= elf2start && elf1end <= elf2end) {
+        resultPartOne += 1
+        resultPartTwo += 1
+    } else if (elf2start >= elf1start && elf2start <= elf1end) || (elf2end >= elf1start && elf2end <= elf1end) {
+        resultPartTwo += 1
     }
-
-    return result
 }
 
-print("PART ONE: \(PartOne())")
-
-// MARK: - PART TWO
-
-func PartTwo() -> Int {
-    var result = 0
-
-    return result
-}
-
-print("PART TWO: \(PartTwo())")
+print("PART ONE: \(resultPartOne)")
+print("PART TWO: \(resultPartTwo)")
