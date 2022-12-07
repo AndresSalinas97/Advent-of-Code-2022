090f38f2d1476a1abe7faed2d7f253eae89ba353
AndresSalinas97 <andressalinas97@outlook.com>
2022-12-07 13:20:56 +0100

    Part two solved

--- Day-07.playground/Contents.swift
+++ Day-07.playground/Contents.swift
@@ -18,20 +18,20 @@ class Directory {
     var name: String
     var files = [File]()
     var directories = [Directory]()
-    
+
     init(name: String) {
         self.name = name
     }
-    
+
     var size: Int {
         var size = 0
-        
+
         files.map { size += $0.size }
         directories.map { size += $0.size }
-        
+
         return size
     }
-    
+
     // For debugging purposes
     func printTree() {
         func printTree(_ dir: Directory, currentLevel: Int) {
@@ -39,14 +39,14 @@ class Directory {
             for _ in 0 ..< currentLevel {
                 spacer += "  "
             }
-            
+
             print("\(spacer)\(dir.name) (dir, size= \(dir.size))")
-            
+
             dir.files.map { print("  \(spacer)\($0.name) (file, size= \($0.size))") }
-            
+
             dir.directories.map { printTree($0, currentLevel: currentLevel + 1) }
         }
-        
+
         printTree(self, currentLevel: 0)
     }
 }
@@ -54,41 +54,55 @@ class Directory {
 func buildFileSystemFromInput() -> Directory {
     var currentPath = [Directory]()
     currentPath.append(Directory(name: "/"))
-    
+
     input.enumerateLines { line, _ in
         if line == "$ cd .." {
             currentPath.popLast()
         } else if line.contains("$ cd "), !(line == "$ cd /") {
             let dirName = line.components(separatedBy: " ")[2]
             let newDir = Directory(name: dirName)
-            
+
             currentPath.last!.directories.append(newDir)
-            
+
             currentPath.append(newDir)
-            
+
         } else if !line.contains("$"), !line.contains("dir ") {
             let fileSize = Int(line.components(separatedBy: " ")[0])!
             let fileName = line.components(separatedBy: " ")[1]
-            
+
             currentPath.last!.files.append(File(name: fileName, size: fileSize))
         }
     }
-    
+
     return currentPath.first!
 }
 
 func getDirectoriesSmallerThan(dir: Directory, size: Int) -> [Directory] {
-    var smallDirectories = [Directory]()
-    
+    var resultArray = [Directory]()
+
     dir.directories.map {
-        smallDirectories += getDirectoriesSmallerThan(dir: $0, size: size)
+        resultArray += getDirectoriesSmallerThan(dir: $0, size: size)
     }
-    
+
     if dir.size < size {
-        smallDirectories.append(dir)
+        resultArray.append(dir)
     }
-    
-    return smallDirectories
+
+    return resultArray
+}
+
+func getDirectoriesBiggerOrEqThan(dir: Directory, size: Int) -> [Directory] {
+    var resultArray = [Directory]()
+
+    dir.directories.map {
+        resultArray += getDirectoriesBiggerOrEqThan(dir: $0, size: size)
+    }
+
+    if dir.size >= size {
+        resultArray.append(dir)
+    }
+
+    return resultArray
 }
 
 var root = buildFileSystemFromInput()
@@ -97,11 +111,30 @@ var root = buildFileSystemFromInput()
 
 func partOne() -> Int {
     var result = 0
-    
-    let smallDirectories = getDirectoriesSmallerThan(dir: root, size: 100000)
+
+    let smallDirectories = getDirectoriesSmallerThan(dir: root, size: 100_000)
     smallDirectories.map { result += $0.size }
-    
+
     return result
 }
 
 print("PART ONE: \(partOne())")
+
+// MARK: - PART TWO
+
+func partTwo() -> Int {
+    let totalSpace = 70_000_000
+    let requiredSpaceForUpdate = 30_000_000
+    let usedSpace = root.size
+    let availableSpace = totalSpace - usedSpace
+    let minSpaceToDelete = requiredSpaceForUpdate - availableSpace
+
+    let optionsToDelete = getDirectoriesBiggerOrEqThan(dir: root, size: minSpaceToDelete)
+
+    var result = optionsToDelete[0].size
+    optionsToDelete.map { result = min(result, $0.size) }
+
+    return result
+}
+
+print("PART TWO: \(partTwo())")
